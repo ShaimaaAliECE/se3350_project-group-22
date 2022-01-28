03635b51c95513532e4ad4087e40e50a8719afb6
Yusra Salam <yusra.salam126@gmail.com>
2022-01-27 21:04:26 -0500

    added a testting button

--- client/src/components/pages/level2page/levelTwo.js
+++ client/src/components/pages/level2page/levelTwo.js
@@ -1,5 +1,15 @@
 import React from "react";
 
-export default class Example extends React.Component {
+export default class LevelTwo extends React.Component {
+    render() {
+        return (
+      
+      <div className="header">
+        <h1>Hello</h1>
+        <button> alice yout button</button>
+
+      </div>
+    );
+    }
   
 }
