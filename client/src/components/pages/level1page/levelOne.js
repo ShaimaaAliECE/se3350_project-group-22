29c4fbcca615a06320c4bbbbe80fbfe8bb719a0a
Mahira <mmoftah3@uwo.ca>
2022-01-28 13:00:31 -0500

    randomgenerator

--- client/src/components/pages/level1page/levelOne.js
+++ client/src/components/pages/level1page/levelOne.js
@@ -1,5 +1,11 @@
-import React from "react";
+// import React from "react";
 
-export default class Example extends React.Component {
-  
+// export default class Example extends React.Component {
+
+// }
+
+function Random() {
+  // var rnd = Math.floor(Math.random() * 20000000000);
+  var rnd = Math.floor(Math.random() * 20) + 1;
+  document.getElementById("tb").value = rnd;
 }
