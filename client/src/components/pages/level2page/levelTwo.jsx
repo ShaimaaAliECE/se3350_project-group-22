cd5f4f9e39e8adc3e337906856ea0da088fd5024
Yusra Salam <yusra.salam126@gmail.com>
2022-01-28 19:11:44 -0500

    Creating a board and making the buttons draggable

--- client/src/components/pages/level2page/levelTwo.jsx
+++ client/src/components/pages/level2page/levelTwo.jsx
@@ -1,23 +1,29 @@
 import React from "react";
 import ReactDOM from 'react-dom';
-import {RandomNumbersArray} from '../../../randomNumberGenerator.js';
+import { RandomNumbersArray } from '../../../randomNumberGenerator.js';
 import Number from '../../Number';
+import Board from "../../Board.js";
+import './level2.css';
+import { useDrag } from 'react-dnd';
+
 
 export default class LevelTwo extends React.Component {
-  getButtonsUsingMap(){
+  getButtonsUsingMap() {
     //create an array of random number
-    const randomNum = RandomNumbersArray(10,20,1);
+    const randomNum = RandomNumbersArray(10, 20, 1);
 
     //return the button with the number in the array
-    let buttons=[];
-    for (let num of randomNum){
-      buttons.push(<Number className={num}></Number>);
+    let buttons = [];
+    for (let num of randomNum) {
+      buttons.push(<Number id={num} className= "number" draggable="true"></Number>);
     }
-        ReactDOM.render(<> 
-             {buttons}
-        </>,document.getElementById("numbers"))
+    ReactDOM.render(<>
+       <Board id='board-1' className='board'> {buttons} </Board> 
+       <Board id='board-2' className='board'> </Board>
+       <Board id='board-2' className='board'> </Board>
+    </>, document.getElementById("numbers"));
   }
-    
+
   render() {
     return (
       <div id="main">
@@ -25,9 +31,13 @@ export default class LevelTwo extends React.Component {
         <button>back</button>
         <button onClick={this.getButtonsUsingMap}>Generate 10 Numbers</button>
         <button>next</button>
-        <div id="numbers"></div>
-        <div id="containers"></div>
+        <div className="flexbox">
+          <div id="numbers"></div>
+          <div id="containers"></div>
         </div>
+
+
+      </div>
     );
   }
 
