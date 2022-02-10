953546397dc2e9e634df1004c7cc05854a5239c0
Yusra Salam <yusra.salam126@gmail.com>
2022-02-10 18:47:14 -0500

    experimenting with level 2

---
+++ client/src/components/pages/level2page/steps.js
@@ -0,0 +1,14 @@
+export const steps = [
+        {
+            "stepID":1,
+            "step": "A set of 10 numbers is randomly generated! Click next!"
+        },
+        {
+            "stepID":2,
+            "step": "Divide the array in half and click verify!"
+        },
+        {
+            "stepID":3,
+            "step": "Divide the set in half again and click verify!"
+        }
+]
