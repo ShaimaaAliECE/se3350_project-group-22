85b16229c45c5d8e88497d93d35be175d88d112c
Yusra Salam <yusra.salam126@gmail.com>
2022-02-10 20:17:58 -0500

    dont take anything from here

--- client/src/components/pages/level2page/levelTwo.jsx
+++ client/src/components/pages/level2page/levelTwo.jsx
@@ -5,18 +5,58 @@ import Number from '../../Number';
 import Board from "../../Board.js";
 import './level2.css';
 import { useDrag } from 'react-dnd';
-
+import { steps } from "./steps";
 
 export default class LevelTwo extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      verify:false,
+      step:0,
+      containers:0,
+      numOfArray:[]
+    };
+
+    this.verify = this.verify.bind(this);
+    this.getButtonNumbers = this.getButtonNumbers.bind(this);
+  }
+  
+  //function to verify if the user drags the right component
+  verify(){
+    alert("it worked");
+    console.log(this.state.step)
+    //check what step we are on
+    if(this.state.step == 1){
+      //check if the containers is 2
+      alert("hi");
+
+    }
+  }
+
   getButtonNumbers() {
     //create an array of random number
     const randomNum = RandomNumbersArray(10, 20, 1);
+    //setting the set stat of numOfArray to whatever randomNum array is so we have it on hand
+    for(var i =0; i < randomNum.length; i++){
+      this.setState({ numOfArray : this.state.numOfArray.push(randomNum[i]) });
+    }
+
+    //setting the step to once since generating the array of number is the first step
+    this.setState({ step : this.state.step++ });
+    console.log(this.state.step);
     //remove comment
     //return the button with the number in the array
     let buttons = [];
     for (let num of randomNum) {
       buttons.push(<Number id={num} className="number" draggable="true"></Number>);
     }
+
+    let boards = [];
+    for(var j = 0; j < 2; j++){
+      boards.push(<Board className='board' id={j}></Board>);
+    }
+    
+    console.log(this.state.numOfArray);
     // render the Number button components in the div called numbers
     ReactDOM.render(<>{buttons}</>, document.getElementById("numbers"));
     ReactDOM.render(<>
@@ -24,35 +64,37 @@ export default class LevelTwo extends React.Component {
       <Board className='board' id='1'></Board>
     </>, document.getElementById("containers"))
     // render the check answer button so that users can check their answers
-    ReactDOM.render(<button onClick={this.checkAns}>check answer</button>, document.getElementById("numbers"));
-  };
-  checkAns() {
-    // randomly choosing if they're right or wrong for now
-    let random = Math.random() * 2;
-    if (random === 1) {
-      ReactDOM.render(<text>you're correct!</text>, document.getElementById("feedback"));
-    } else {
-      ReactDOM.render(<text>you're wrong!</text>, document.getElementById("feedback"));
-    }
+    // ReactDOM.render(<button onClick={this.checkAns}>check answer</button>, document.getElementById("numbers"));
   };
 
-  getPrev() {
-    let xReq = new XMLHttpRequest();              // creating an XMLHttp Request object
-    xReq.onreadystatechange = showNext;              // when the state changes, display the generated numbers
-    // first argument is HTTP method, second is URL to send req to, third is syn/async spec (true = async)
-    xReq.open('GET', '/check-answer', true);   // submit request to generate 10 numbers from 1-20
-    // send request
-    xReq.send();
-  };
 
-  getNext() {
-    let xReq = new XMLHttpRequest();              // creating an XMLHttp Request object
-    xReq.onreadystatechange = showNext;              // when the state changes, display the generated numbers
-    // first argument is HTTP method, second is URL to send req to, third is syn/async spec (true = async)
-    xReq.open('GET', '/get-next', true);   // submit request to generate 10 numbers from 1-20
-    // send request
-    xReq.send();
-  };
+  // checkAns() {
+  //   // randomly choosing if they're right or wrong for now
+  //   let random = Math.random() * 2;
+  //   if (random === 1) {
+  //     ReactDOM.render(<text>you're correct!</text>, document.getElementById("feedback"));
+  //   } else {
+  //     ReactDOM.render(<text>you're wrong!</text>, document.getElementById("feedback"));
+  //   }
+  // };
+
+  // getPrev() {
+  //   let xReq = new XMLHttpRequest();              // creating an XMLHttp Request object
+  //   xReq.onreadystatechange = showNext;              // when the state changes, display the generated numbers
+  //   // first argument is HTTP method, second is URL to send req to, third is syn/async spec (true = async)
+  //   xReq.open('GET', '/check-answer', true);   // submit request to generate 10 numbers from 1-20
+  //   // send request
+  //   xReq.send();
+  // };
+
+  // getNext() {
+  //   let xReq = new XMLHttpRequest();              // creating an XMLHttp Request object
+  //   xReq.onreadystatechange = showNext;              // when the state changes, display the generated numbers
+  //   // first argument is HTTP method, second is URL to send req to, third is syn/async spec (true = async)
+  //   xReq.open('GET', '/get-next', true);   // submit request to generate 10 numbers from 1-20
+  //   // send request
+  //   xReq.send();
+  // };
 
   render() {
     return (
@@ -61,6 +103,7 @@ export default class LevelTwo extends React.Component {
         <button onClick={this.getPrev}>back</button>
         <button onClick={this.getButtonNumbers}>Generate 10 Numbers</button>
         <button onClick={this.getNext}>next</button>
+        
         <div className="flexbox">
           <div id="numbers">
 
@@ -69,6 +112,7 @@ export default class LevelTwo extends React.Component {
 
           </div>
         </div>
+        <button onClick={this.verify}>Verify</button>
         <div id="feedback"></div>
         <div id="step"></div>
       </div>
