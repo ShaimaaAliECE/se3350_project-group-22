082ba3310bdb54de7feb5591f7ec08750a5c6277
Isabel <ihiggon@uwo.ca>
2022-02-10 21:24:03 -0500

    base level 3 page

--- client/src/components/pages/level3page/levelThree.js
+++ client/src/components/pages/level3page/levelThree.js
@@ -142,6 +142,208 @@ export default class LevelThree extends Component {
                 readOnly
               />
             </div>
+
+            {/* step 2 number block */}
+            <div className="blockofnum">
+              <input type="number" id="n1" name="n1" size="1" readOnly />
+              <input type="number" id="n2" name="n2" size="1" readOnly />
+              <input type="number" id="n3" name="n3" size="1" readOnly />
+              <input type="number" id="n4" name="n4" size="1" readOnly />
+              <input type="number" id="n5" name="n2" size="1" readOnly />
+
+              <span style={{ display: "inline-block", width: "200px" }}></span>
+
+              <input type="number" id="n6" name="n2" size="1" readOnly />
+              <input type="number" id="n7" name="n2" size="1" readOnly />
+              <input type="number" id="n8" name="n2" size="1" readOnly />
+              <input type="number" id="n9" name="n2" size="1" readOnly />
+              <input type="number" id="n10" name="n2" size="1" readOnly />
+            </div>
+
+            {/* step 3 block */}
+            <div className="blockofnum">
+              <input type="number" id="num1" name="n1" size="1" readOnly />
+              <input type="number" id="num2" name="n2" size="1" readOnly />
+              <input type="number" id="num3" name="n3" size="1" readOnly />
+
+              <span style={{ display: "inline-block", width: "70px" }}></span>
+
+              <input type="number" id="num4" name="n4" size="1" readOnly />
+              <input type="number" id="num5" name="n4" size="1" readOnly />
+
+              <span style={{ display: "inline-block", width: "100px" }}></span>
+
+              <input type="number" id="num6" name="n1" size="1" readOnly />
+              <input type="number" id="num7" name="n2" size="1" readOnly />
+              <input type="number" id="num8" name="n2" size="1" readOnly />
+
+              <span style={{ display: "inline-block", width: "100px" }}></span>
+
+              <input type="number" id="num9" name="n2" size="1" readOnly />
+              <input type="number" id="num10" name="n2" size="1" readOnly />
+            </div>
+
+
+            {/* step 4 block */}
+            <div className="blockofnum">
+              {/* <!-- group 1 -- > */}
+              <input type="number" id="n1Step4" name="n1" size="1" readOnly />
+              <input type="number" id="n2Step4" name="n2" size="1" readOnly />
+              <span style={{ display: "inline-block", width: "60px" }}></span>
+              {/* <!--group 2 -- > */}
+              <input type="number" id="n3Step4" name="n3" size="1" readOnly />
+              <span style={{ display: "inline-block", width: "50px" }}></span>
+              {/* <!--group 3 -- > */}
+              <input type="number" id="n4Step4" name="n4" size="1" readOnly />
+              <span style={{ display: "inline-block", width: "20px" }}></span>
+              <input type="number" id="n5Step4" name="n4" size="1" readOnly />
+              {/* <!--group 3 -- > */}
+              <span style={{ display: "inline-block", width: "80px" }}></span>
+              <input type="number" id="n6Step4" name="n1" size="1" readOnly />
+              <input type="number" id="n7Step4" name="n2" size="1" readOnly />
+              <span style={{ display: "inline-block", width: "20px" }}></span>
+              {/* <!--   group 4 -- > */}
+              <input type="number" id="n8Step4" name="n2" size="1" readOnly />
+
+              <span style={{ display: "inline-block", width: "80px" }}></span>
+              {/* <!--group 5 -- > */}
+              <input type="number" id="n9Step4" name="n2" size="1" readOnly />
+              <span style={{ display: "inline-block", width: "20px" }}></span>
+              {/* <!--group 7 -- > */}
+              <input type="number" id="n10Step4" name="n2" size="1" readOnly />
+            </div>
+
+
+            {/* step 5 block */}
+            <div className="blockofnum">
+              {/* <!--
+            group 1 -- > */}
+              <input type="number" id="n1Step5" name="n1" size="1" readOnly />
+              <span style={{ display: "inline-block", width: "15px" }}></span>
+              <input type="number" id="n2Step5" name="n2" size="1" readOnly />
+              <span style={{ display: "inline-block", width: "60px" }}></span>
+              {/* <!--group 2 -- > */}
+              <input type="number" id="n3Step5" name="n3" size="1" readOnly />
+              <span style={{ display: "inline-block", width: "50px" }}></span>
+              {/* <!--group 3 -- > */}
+              <input type="number" id="n4Step5" name="n4" size="1" readOnly />
+              <span style={{ display: "inline-block", width: "20px" }}></span>
+              <input type="number" id="n5Step5" name="n4" size="1" readOnly />
+
+              {/* <!--group 3 -- > */}
+              <span style={{ display: "inline-block", width: "60px" }}></span>
+              <input type="number" id="n6Step5" name="n1" size="1" readOnly />
+              <span style={{ display: "inline-block", width: "20px" }}></span>
+              <input type="number" id="n7Step5" name="n2" size="1" readOnly />
+              <span style={{ display: "inline-block", width: "20px" }}></span>
+              {/* <!--
+            group 4 -- > */}
+              <input type="number" id="n8Step5" name="n2" size="1" readOnly />
+
+              <span style={{ display: "inline-block", width: "80px" }}></span>
+              {/* <!--group 5 -- > */}
+              <input type="number" id="n9Step5" name="n2" size="1" readOnly />
+              <span style={{ display: "inline-block", width: "20px" }}></span>
+              {/* <!--group 7 -- > */}
+              <input type="number" id="n10Step5" name="n2" size="1" readOnly />
+            </div>
+
+
+            {/* step 6 block */}
+            <div className="blockofnum">
+              {/* <!--group 1 -- > */}
+              <input type="number" id="n1Step6" name="n1" size="1" readOnly />
+              <input type="number" id="n2Step6" name="n2" size="1" readOnly />
+              <span style={{ display: "inline-block", width: "80px" }}></span>
+              {/* <!--group 2 -- > */}
+              <input type="number" id="n3Step6" name="n3" size="1" readOnly />
+              <span style={{ display: "inline-block", width: "80px" }}></span>
+              {/* <!--group 3 -- > */}
+              <input type="number" id="n4Step6" name="n4" size="1" readOnly />
+              <input type="number" id="n5Step6" name="n5" size="1" readOnly />
+
+              {/* <!--group 3 -- > */}
+              <span style={{ display: "inline-block", width: "90px" }}></span>
+              <input type="number" id="n6Step6" name="n1" size="1" readOnly />
+              <input type="number" id="n7Step6" name="n2" size="1" readOnly />
+              <span style={{ display: "inline-block", width: "90px" }}></span>
+              {/* <!--
+            group 4 -- > */}
+              <input type="number" id="n8Step6" name="n2" size="1" readOnly />
+
+              <span style={{ display: "inline-block", width: "80px" }}></span>
+              {/* <!--group 5 -- > */}
+              <input type="number" id="n9Step6" name="n2" size="1" readOnly />
+              {/* <!--group 7 -- > */}
+              <input type="number" id="n10Step6" name="n2" size="1" readOnly />
+            </div>
+
+
+            {/* step 7 block */}
+            <div className="blockofnum">
+              <input type="number" id="n1Step7" name="n1" size="1" readOnly />
+              <input type="number" id="n2Step7" name="n2" size="1" readOnly />
+              <input type="number" id="n3Step7" name="n3" size="1" readOnly />
+              {/* <!--group 2 -- > */}
+
+              <span style={{ display: "inline-block", width: "80px" }}></span>
+              {/* <!--group 3 -- > */}
+              <input type="number" id="n4Step7" name="n4" size="1" readOnly />
+              <input type="number" id="n5Step7" name="n5" size="1" readOnly />
+
+              {/* <!--group 3 -- > */}
+              <span style={{ display: "inline-block", width: "100px" }}></span>
+              <input type="number" id="n6Step7" name="n1" size="1" readOnly />
+              <input type="number" id="n7Step7" name="n2" size="1" readOnly />
+              <input type="number" id="n8Step7" name="n2" size="1" readOnly />
+              <span style={{ display: "inline-block", width: "40px" }}></span>
+              {/* <!--
+            group 4 -- > */}
+
+              <span style={{ display: "inline-block", width: "80px" }}></span>
+              {/* <!--group 5 -- > */}
+              <input type="number" id="n9Step7" name="n2" size="1" readOnly />
+              {/* <!--group 7 -- > */}
+              <input type="number" id="n10Step7" name="n2" size="1" readOnly />
+            </div>
+
+            {/* step 8 block */}
+            <div className="blockofnum">
+              <input type="number" id="n1Step8" name="n1" size="1" readOnly />
+              <input type="number" id="n2Step8" name="n2" size="1" readOnly />
+              <input type="number" id="n3Step8" name="n3" size="1" readOnly />
+              <input type="number" id="n4Step8" name="n4" size="1" readOnly />
+              <input type="number" id="n5Step8" name="n5" size="1" readOnly />
+
+              {/* <!--group 3 -- > */}
+              <span style={{ display: "inline-block", width: "180px" }}></span>
+              <input type="number" id="n6Step8" name="n1" size="1" readOnly />
+              <input type="number" id="n7Step8" name="n2" size="1" readOnly />
+              <input type="number" id="n8Step8" name="n2" size="1" readOnly />
+
+              <input type="number" id="n9Step8" name="n2" size="1" readOnly />
+              {/* <!--group 7 -- > */}
+              <input type="number" id="n10Step8" name="n2" size="1" readOnly />
+            </div>
+
+
+            {/* step 9 block */}
+            <div className="blockofnum">
+              <input type="number" id="n1Step9" name="n1" size="1" readOnly />
+              <input type="number" id="n2Step9" name="n2" size="1" readOnly />
+              <input type="number" id="n3Step9" name="n3" size="1" readOnly />
+              <input type="number" id="n4Step9" name="n4" size="1" readOnly />
+              <input type="number" id="n5Step9" name="n5" size="1" readOnly />
+              <input type="number" id="n6Step9" name="n1" size="1" readOnly />
+              <input type="number" id="n7Step9" name="n2" size="1" readOnly />
+              <input type="number" id="n8Step9" name="n2" size="1" readOnly />
+
+              <input type="number" id="n9Step9" name="n2" size="1" readOnly />
+              {/* //    <!--group 7 -- > */}
+              <input type="number" id="n10Step9" name="n2" size="1" readOnly />
+            </div>
+
+
           </div>
           );{" "}
         </form>
