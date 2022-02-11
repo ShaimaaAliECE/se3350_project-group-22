e5e009231d6b313a01ca7653a7fac3926f93365d
Alice Sung <alicesung2001@gmail.com>
2022-02-10 20:17:11 -0500

    steps

--- client/src/components/pages/level2page/levelTwo.jsx
+++ client/src/components/pages/level2page/levelTwo.jsx
@@ -6,12 +6,32 @@ import Board from "../../Board.js";
 import './level2.css';
 import {useDrag} from 'react-dnd';
 
+// declare var to keep track of the step we're on, and start on step one.
+let count = 0; 
 
 export default class LevelTwo extends React.Component {
+
+
+  incrStep(){
+    if (count < 9){
+      count++;
+    }
+    ReactDOM.render(<><text>step: </text>{count}</>, document.getElementById("step"));
+
+
+
+  }
+
+  decrStep(){
+    if (count > 1){
+      count--;
+    }
+    ReactDOM.render(<><text>step: </text>{count}</>, document.getElementById("step"));
+  }
+
   getButtonNumbers() {
     //create an array of random number
     const randomNum = RandomNumbersArray(10, 20, 1);
-    //remove comment
     //return the button with the number in the array
     let buttons = [];
     for (let num of randomNum) {
@@ -21,65 +41,60 @@ export default class LevelTwo extends React.Component {
     ReactDOM.render(<>{buttons}</>, document.getElementById("numbers"));
     ReactDOM.render(<>
       <Board className='board' id='1'></Board>
-      <Board className='board' id='1'></Board>
-    </>, document.getElementById("containers"))
+      <Board className='board' id='2'></Board>
+    </>, document.getElementById("containers"));
     // render the check answer button so that users can check their answers
     ReactDOM.render(<button onClick={this.checkAns}>check answer</button>, document.getElementById("numbers"));
   };
-  checkAns() {
-    // randomly choosing if they're right or wrong for now
-    let random = Math.random() * 2;
-    if (random === 1) {
-      ReactDOM.render(<text>you're correct!</text>, document.getElementById("feedback"));
-    } else {
-      ReactDOM.render(<text>you're wrong!</text>, document.getElementById("feedback"));
-    }
-  };
+  // checkAns() {
+  //   // randomly choosing if they're right or wrong for now
+  //   let random = Math.random() * 2;
+  //   if (random === 1) {
+  //     ReactDOM.render(<text>you're correct!</text>, document.getElementById("feedback"));
+  //   } else {
+  //     ReactDOM.render(<text>you're wrong!</text>, document.getElementById("feedback"));
+  //   }
+  // };
 
-  getPrev() {
-    let xReq = new XMLHttpRequest();              // creating an XMLHttp Request object
-    xReq.onreadystatechange = showNext;              // when the state changes, display the generated numbers
-    // first argument is HTTP method, second is URL to send req to, third is syn/async spec (true = async)
-    xReq.open('GET', '/check-answer', true);   // submit request to generate 10 numbers from 1-20
-    // send request
-    xReq.send();
-  };
+  // getPrev() {
+  //   let xReq = new XMLHttpRequest();              // creating an XMLHttp Request object
+  //   xReq.onreadystatechange = showNext;              // when the state changes, display the generated numbers
+  //   // first argument is HTTP method, second is URL to send req to, third is syn/async spec (true = async)
+  //   xReq.open('GET', '/check-answer', true);   // submit request to generate 10 numbers from 1-20
+  //   // send request
+  //   xReq.send();
+  // };
 
-  getNext() {
-    let xReq = new XMLHttpRequest();              // creating an XMLHttp Request object
-    xReq.onreadystatechange = showNext;              // when the state changes, display the generated numbers
-    // first argument is HTTP method, second is URL to send req to, third is syn/async spec (true = async)
-    xReq.open('GET', '/get-next', true);   // submit request to generate 10 numbers from 1-20
-    // send request
-    xReq.send();
-  };
+  // getNext() {
+  //   let xReq = new XMLHttpRequest();              // creating an XMLHttp Request object
+  //   xReq.onreadystatechange = showNext;              // when the state changes, display the generated numbers
+  //   // first argument is HTTP method, second is URL to send req to, third is syn/async spec (true = async)
+  //   xReq.open('GET', '/get-next', true);   // submit request to generate 10 numbers from 1-20
+  //   // send request
+  //   xReq.send();
+  // };
 
   render() {
     return (
       <div id="main">
         <h1>Level Two</h1>
-        <button onClick={this.getPrev}>back</button>
+        <button onClick={this.decrStep}>back</button>
         <button onClick={this.getButtonNumbers}>Generate 10 Numbers</button>
-        <button onClick={this.getNext}>next</button>
-        <div className="flexbox">
-          <div id="numbers">
-
-          </div>
-          <div id="containers">
-
-          </div>
-        </div>
-        <div id="feedback"></div>
+        <button onClick={this.incrStep}>next</button>
+        <div className="flexbox"> <div id="numbers"></div>  </div>
+        <div className="flexbox"> <div id="containers"></div> </div>
         <div id="step"></div>
+        <div id="check"></div>
+        <div id="feedback"></div>
       </div>
     );
   }
 }
 
 
-// getting the question stem
-function showNext() {
-  let stepDiv = document.getElementById("step");
-  stepDiv.innerHTML = '<text>' + this.responseText + '</text><br><br><button href="/levelTwo/">click to restart</button>';
-}
+// // getting the question stem
+// function showNext() {
+//   let stepDiv = document.getElementById("step");
+//   stepDiv.innerHTML = '<text>' + this.responseText + '</text><br><br><button href="/levelTwo/">click to restart</button>';
+// }
 
