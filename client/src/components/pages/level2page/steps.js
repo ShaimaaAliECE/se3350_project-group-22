f8b77c2e1a011e5ea7f0250176323035bffd513d
Yusra Salam <yusra.salam126@gmail.com>
2022-02-10 20:08:33 -0500

    edit steps file

--- client/src/components/pages/level2page/steps.js
+++ client/src/components/pages/level2page/steps.js
@@ -1,14 +1,42 @@
 export const steps = [
         {
             "stepID":1,
-            "step": "A set of 10 numbers is randomly generated! Click next!"
+            "step": "Divide the array in half and click verify! Click next!",
+            "container": 2
         },
         {
             "stepID":2,
-            "step": "Divide the array in half and click verify!"
+            "step": "Divide the set in half again and click verify!",
+            "container": 4
         },
         {
             "stepID":3,
-            "step": "Divide the set in half again and click verify!"
+            "step": "Divide the set in half again and click verify!",
+            "container": 8
+        },
+        {
+            "stepID":4,
+            "step": "Divide the set in half again and click verify!",
+            "container": 10
+        },
+        {
+            "stepID":5,
+            "step": "Merge the numbers that are ready to be merged into pairs while recusively ordering them in ascending order. Click verify!",
+            "container": 6
+        },
+        {
+            "stepID":6,
+            "step": "Continue merging the numbers that are ready to be merged together while recusively ordering them in ascending order. Click verify!",
+            "container": 4
+        },
+        {
+            "stepID":7,
+            "step": "Continue merging the numbers that are ready to be merged together while recusively ordering them in ascending order. Click verify!",
+            "container": 2
+        },
+        {
+            "stepID":8,
+            "step": "Merge the 2 groups together in ascending order. This is the end of the algorithm. Click verify!",
+            "container": 1
         }
 ]
