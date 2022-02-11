0b417ee31365ee6aa41e05a94f064107836ea2c5
Mahira <mmoftah3@uwo.ca>
2022-02-10 20:10:37 -0500

    level 3 base

---
+++ client/src/components/pages/level3page/Level3Functions.js
@@ -0,0 +1,78 @@
+export function Random() {
+  var size = 10;
+  var max = 20;
+  var min = 1;
+  var numbersArray = new Array(size);
+  numbersArray = RandomNumbersArray(size, max, min);
+
+  //show the steps
+  var show1 = document.getElementById("showStep1");
+  show1.classList.toggle("show");
+  document.getElementById("step1").value =
+    "Step1: A set of 10 numbers is randomly generated!";
+  //random assign stuff
+  var rnd = numbersArray[0];
+  var rnd2 = numbersArray[1];
+  var rnd3 = numbersArray[2];
+  var rnd4 = numbersArray[3];
+  var rnd5 = numbersArray[4];
+  var rnd6 = numbersArray[5];
+  var rnd7 = numbersArray[6];
+  var rnd8 = numbersArray[7];
+  var rnd9 = numbersArray[8];
+  var rnd10 = numbersArray[9];
+
+  document.getElementById("tb").value = rnd;
+  document.getElementById("tb2").value = rnd2;
+  document.getElementById("tb3").value = rnd3;
+  document.getElementById("tb4").value = rnd4;
+  document.getElementById("tb5").value = rnd5;
+  document.getElementById("tb6").value = rnd6;
+  document.getElementById("tb7").value = rnd7;
+  document.getElementById("tb8").value = rnd8;
+  document.getElementById("tb9").value = rnd9;
+  document.getElementById("tb10").value = rnd10;
+}
+
+function RandomNumbersArray(size, max, min) {
+  var count = 1;
+  var rangeSize = max - min + 1;
+  var numbersArray = new Array(size);
+  var randomNum = 0;
+
+  // populate the array
+  for (var j = 0; j < size; j++) {
+    numbersArray[j] = 0;
+  }
+
+  for (var i = 0; i < size; i++) {
+    do {
+      randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
+    } while (count <= rangeSize && InNumberList(numbersArray, randomNum, size));
+
+    numbersArray[i] = randomNum;
+    count++;
+  }
+
+  return numbersArray;
+}
+
+function InNumberList(numbersArray, randomNum, size) {
+  // check the numbers to make sure it is not already in the array
+  for (var j = 0; j < size; j++) {
+    if (numbersArray[j] == randomNum) {
+      return true;
+    }
+  }
+  return false;
+}
+
+export function Next() {
+  var show = document.getElementById("showStep1");
+  show.classList.toggle("show");
+}
+//    <div className="footer"></div>
+//         <h1>
+//           Hello
+//           <button onClick={this.playSound}>Play Audio Feedback Sample</button>
+//         </h1>
--- client/src/components/pages/level3page/levelThree.js
+++ client/src/components/pages/level3page/levelThree.js
@@ -1,66 +1,141 @@
-  import React, { Component } from "react";
-  import incorrectSfx from '../../../sounds/wrong.mp3';
-  import correctSfx from '../../../sounds/correct.mp3';
-  export default class LevelThree extends Component {
-
+import React, { Component } from "react";
+import incorrectSfx from "../../../sounds/wrong.mp3";
+import correctSfx from "../../../sounds/correct.mp3";
+import styles from "./levelThree.css";
+import { Random, Next } from "./Level3Functions";
+export default class LevelThree extends Component {
   // verify user response
-  checkResponse = async e => {       
-
-  e.preventDefault();
-    const response = await fetch('/check-answer', {
-      method: 'GET',      
-      headers: { 'Content-Type': 'application/json' },   
+  checkResponse = async (e) => {
+    e.preventDefault();
+    const response = await fetch("/check-answer", {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
     });
-      
+
     const body = await response.text();
-      alert (body);
-      console.log (body);   
-      this.setState({ userResponse: body });
-    };
+    alert(body);
+    console.log(body);
+    this.setState({ userResponse: body });
+  };
 
-    state = {
+  state = {
+    // Get audio files in a variable
+    incorrectSound: new Audio(incorrectSfx),
+    correctSound: new Audio(correctSfx),
 
-      // Get audio files in a variable
-      incorrectSound: new Audio(incorrectSfx),
-      correctSound: new Audio(correctSfx),
+    // State for user response
+    incorrect: true,
+    correct: false,
+  };
 
-      // State for user response
-      incorrect: true,
-      correct: false,
-    };
+  playSound = () => {
+    // Get state of user response
+    let incorrect = this.state.incorrect;
+    let correct = this.state.correct;
 
-    playSound = () => {
-      // Get state of user response
-      let incorrect = this.state.incorrect;
-      let correct = this.state.correct;
-
-      if (incorrect && !correct) {
-        // Play correct sound if user response is right
-        this.state.incorrectSound.pause();
-        this.state.correctSound.play();
-      }
-      else {
-        // Play incorrect sound if user response is wrong
-        this.state.correctSound.pause();
-        this.state.incorrectSound.play();
-      }
-
-        // Change the state of user answer later
-      this.setState({ incorrect: !incorrect });
-        this.setState({ correct: !correct });
-    };
-
-    render() {
-      return (
-
-        <div className="header">
-          <h1>Hello
-            <button onClick={this.playSound}>
-              Play Audio Feedback Sample
-            </button>
-            </h1>
-        </div>
-    );
+    if (incorrect && !correct) {
+      // Play correct sound if user response is right
+      this.state.incorrectSound.pause();
+      this.state.correctSound.play();
+    } else {
+      // Play incorrect sound if user response is wrong
+      this.state.correctSound.pause();
+      this.state.incorrectSound.play();
     }
-  }
 
+    // Change the state of user answer later
+    this.setState({ incorrect: !incorrect });
+    this.setState({ correct: !correct });
+  };
+
+  render() {
+    return (
+      <div>
+        <div className="header1">
+          <h1>Level 3: MergeSort Algorithm</h1>
+        </div>
+        <div>
+          <button className="home">Home</button>
+          <span style={{ display: "inline-block", width: "20px" }}></span>
+          <button className="back">Back</button>
+          <span style={{ display: "inline-block", width: "20px" }}></span>
+          {/* 
+                  {/* <!-- button for first step  --> */}
+          <button id="next" className="home" value="Start" onClick={Random}>
+            Next
+          </button>
+          <span style={{ display: "inline-block", width: "20px" }}></span>
+          <button className="restart">Restart</button>
+          <span style={{ display: "inline-block", width: "20px" }}></span>
+          <button className="quit">Quit</button>
+        </div>
+        <div className="instruction1">
+          <h3> Click on the button to Start</h3>
+        </div>
+        <div className="randomdiv">
+          <input
+            type="button"
+            className="random"
+            value="Random Number"
+            onClick={Random}
+          />
+        </div>
+        <form name="rn">
+          {/* <!--step1 --> */}
+          <div className="hide" id="showStep1">
+            <input
+              type="text"
+              id="step1"
+              name="step1"
+              size="80"
+              style={{
+                flex: 1,
+                width: "100%",
+                height: 50,
+                color: "#FFF",
+                textAlignVertical: "top", // android fix for centering it at the top-left corner
+              }}
+              readOnly
+            />
+
+            <div className="blockofnum">
+              <input type="number" id="tb" value="Random Number!" readOnly />
+              <input type="number" id="tb2" value="Random Number!" readOnly />
+              <input type="number" id="tb3" value="Random Number!" readOnly />
+              <input type="number" id="tb4" value="Random Number!" readOnly />
+              <input type="number" id="tb5" value="Random Number!" readOnly />
+              <input type="number" id="tb6" value="Random Number!" readOnly />
+              <input type="number" id="tb7" value="Random Number!" readOnly />
+              <input type="number" id="tb8" value="Random Number!" readOnly />
+              <input type="number" id="tb9" value="Random Number!" readOnly />
+              <input type="number" id="tb10" value="Random Number!" readOnly />
+            </div>
+            <h3>Choose your next step</h3>
+            <input
+              type="button"
+              className="nextStep"
+              value="Merge Array"
+              //go to the next step
+              onClick={this.playSound}
+            />
+            <span style={{ display: "inline-block", width: "20px" }}></span>
+            <input
+              type="button"
+              className="nextStep"
+              value="Split Array"
+              //go to the next step
+              onClick={Next}
+            />
+          </div>
+          );{" "}
+        </form>
+        <div className="footer"></div>
+        <div className="footer"></div>{" "}
+        <h1>
+          Hello{" "}
+          <button onClick={this.playSound}>Play Audio Feedback Sample</button>{" "}
+        </h1>
+      </div>
+    );
+  }
+}
