2296b3a5f9e576ae0bb6c14d1d21c1ed03fa74cd
Yusra Salam <yusra.salam126@gmail.com>
2022-02-12 21:51:58 -0500

    editted the verify method

--- client/src/components/pages/level2page/levelTwo.jsx
+++ client/src/components/pages/level2page/levelTwo.jsx
@@ -73,7 +73,7 @@ export default class LevelTwo extends React.Component {
     for(var b = 0; b < currentContainers; b++){
       for(var c = 0; c < steps[currentStep-1].numInCon[b]; c++){ //in the js file for steps have ex under step 1 numberscontained : [5,5]
         if(boardContainers[b].children[c] != steps[currentStep-1].numInCon[b]){
-          
+          console.log("BITCHHH you wrong afff");
         }
       //checking if the array stored is the same as the users array.
         if(numOfArray[index] == boardContainers[b].children[c].id){
