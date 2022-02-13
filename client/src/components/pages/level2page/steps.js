399709e5bee30af41d1389255fc2647dbc751b1f
Yusra Salam <yusra.salam126@gmail.com>
2022-02-12 20:56:41 -0500

    edit the steps file

--- client/src/components/pages/level2page/steps.js
+++ client/src/components/pages/level2page/steps.js
@@ -2,41 +2,49 @@ export const steps = [
         {
             "stepID":1,
             "step": "Divide the array in half and click verify! Click next!",
-            "container": 2
+            "container": 2,
+            "numInCon" : [5,5]
         },
         {
             "stepID":2,
             "step": "Divide the set in half again and click verify!",
-            "container": 4
+            "container": 4,
+            "numInCon" : [3,2,3,2]
         },
         {
             "stepID":3,
             "step": "Divide the set in half again and click verify!",
-            "container": 8
+            "container": 8,
+            "numInCon" : [2,1,1,1,2,1,1,1]
         },
         {
             "stepID":4,
             "step": "Divide the set in half again and click verify!",
-            "container": 10
+            "container": 10,
+            "numInCon" : [1,1,1,1,1,1,1,1,1,1]
         },
         {
             "stepID":5,
             "step": "Merge the numbers that are ready to be merged into pairs while recusively ordering them in ascending order. Click verify!",
-            "container": 6
+            "container": 6,
+            "numInCon" : [2,1,2,2,1,2]
         },
         {
             "stepID":6,
             "step": "Continue merging the numbers that are ready to be merged together while recusively ordering them in ascending order. Click verify!",
-            "container": 4
+            "container": 4,
+            "numInCon" : [3,2,3,2]
         },
         {
             "stepID":7,
             "step": "Continue merging the numbers that are ready to be merged together while recusively ordering them in ascending order. Click verify!",
-            "container": 2
+            "container": 2,
+            "numInCon" : [5,5]
         },
         {
             "stepID":8,
             "step": "Merge the 2 groups together in ascending order. This is the end of the algorithm. Click verify!",
-            "container": 1
+            "container": 1,
+            "numInCon" : [10]
         }
 ]
