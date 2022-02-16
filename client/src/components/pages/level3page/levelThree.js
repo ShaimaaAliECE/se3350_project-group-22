c124dacf6e7f313882e9534440ba45e5d742a98b
AndrewLou2 <alou2@uwo.ca>
2022-02-16 17:52:22 -0500

    DnD template

--- client/src/components/pages/level3page/levelThree.js
+++ client/src/components/pages/level3page/levelThree.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import incorrectSfx from "../../../sounds/wrong.mp3";
 import correctSfx from "../../../sounds/correct.mp3";
 import styles from "./levelThree.css";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+
 import {
   Random,
   checkFeedbackCorrect,
@@ -17,7 +19,71 @@ import {
   Next5,
   Next1,
 } from "./Level3Functions";
+
+// fake data generator
+const getItems = (count) =>
+  Array.from({ length: count }, (v, k) => k).map((k) => ({
+    id: `item-${k}`,
+    content: `item ${k}`,
+  }));
+
+// a little function to help us with reordering the result
+const reorder = (list, startIndex, endIndex) => {
+  const result = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
+
+  return result;
+};
+
+const grid = 10;
+
+const getItemStyle = (isDragging, draggableStyle) => ({
+  // some basic styles to make the items look a bit nicer
+  userSelect: "none",
+  padding: grid * 2,
+  margin: `0 ${grid}px 0 0`,
+
+  // change background colour if dragging
+  background: isDragging ? "lightgreen" : "grey",
+
+  // styles we need to apply on draggables
+  ...draggableStyle,
+});
+
+const getListStyle = (isDraggingOver) => ({
+  background: isDraggingOver ? "lightblue" : "lightgrey",
+  display: "flex",
+  padding: grid,
+  overflow: "auto",
+});
+
 export default class LevelThree extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: getItems(6),
+    };
+    this.onDragEnd = this.onDragEnd.bind(this);
+  }
+
+  onDragEnd(result) {
+    // dropped outside the list
+    if (!result.destination) {
+      return;
+    }
+
+    const items = reorder(
+      this.state.items,
+      result.source.index,
+      result.destination.index
+    );
+
+    this.setState({
+      items,
+    });
+  }
+
   // verify user response
   checkResponse = async (e) => {
     e.preventDefault();
@@ -587,10 +653,37 @@ export default class LevelThree extends Component {
         </form>
         <div className="footer"></div>
         <div className="footer"></div>{" "}
-        <h1>
-          Hello{" "}
-          <button onClick={this.playSound}>Play Audio Feedback Sample</button>{" "}
-        </h1>
+        <button onClick={this.playSound}>Play Audio Feedback Sample</button>{" "}
+        <DragDropContext onDragEnd={this.onDragEnd}>
+          <Droppable droppableId="droppable" direction="horizontal">
+            {(provided, snapshot) => (
+              <div
+                ref={provided.innerRef}
+                style={getListStyle(snapshot.isDraggingOver)}
+                {...provided.droppableProps}
+              >
+                {this.state.items.map((item, index) => (
+                  <Draggable key={item.id} draggableId={item.id} index={index}>
+                    {(provided, snapshot) => (
+                      <div
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                        style={getItemStyle(
+                          snapshot.isDragging,
+                          provided.draggableProps.style
+                        )}
+                      >
+                        {item.content}
+                      </div>
+                    )}
+                  </Draggable>
+                ))}
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+        </DragDropContext>
       </div>
     );
   }
